525eac70cce57566fc639831f496ca9c88817d77
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-03-15 17:34:38 +0000

    Implementación que elimina registros.

--- trunk/app-web/src/cl/buildersoft/web/servlet/table/DeleteRecords.java
+++ trunk/app-web/src/cl/buildersoft/web/servlet/table/DeleteRecords.java
@@ -1,14 +1,19 @@
 package cl.buildersoft.web.servlet.table;
 
 import java.io.IOException;
+import java.sql.Connection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cl.buildersoft.framework.beans.BSTableConfig;
+import cl.buildersoft.framework.database.BSmySQL;
 
 @WebServlet("/admin/DeleteRecords")
-public class DeleteRecords extends HttpServlet {
+public class DeleteRecords extends AbstractServletUtil {
 
 	private static final long serialVersionUID = -2340853411641380529L;
 
@@ -25,8 +30,54 @@ public class DeleteRecords extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession session = request.getSession();
+		BSTableConfig table = null;
+		synchronized (session) {
+			table = (BSTableConfig) session.getAttribute("BSTable");
+		}
+
+		String idField = getIdField(table.getFields()).getName();
+		String sql = getSQL4Search(table, idField);
+
+		String[] values = request.getParameterValues(idField);
+
+		Connection conn = null;
+		BSmySQL mySQL = new BSmySQL();
+		try {
+			conn = mySQL.getConnection(getServletContext(), "bsframework");
+			for (String value : values) {
+				Long id = Long.parseLong(value);
+
+				mySQL.update(conn, sql, array2List(id));
+
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		//
+		//
+		// ResultSet rs = mySQL.queryResultSet(conn, sql, array2List(id));
+		// resultset2Table(rs, table);
+		//
+		// request.setAttribute("Data", rs);
+		// } catch (Exception e) {
+		// throw new RuntimeException(e);
+		// }
+		// request.setAttribute("Action", "Update");
+
+		// request.getRequestDispatcher("/ShowParameters").forward(request,
+		// response);
 		request.getRequestDispatcher("/admin/LoadTable").forward(request,
 				response);
+	}
+
+	private String getSQL4Search(BSTableConfig table, String idField) {
+		// BSField[] fields = table.getFields();
+		String sql = "DELETE FROM ";
+		sql += table.getTableName();
+		sql += " WHERE " + idField + "=?";
+		return sql;
 	}
 
 }
