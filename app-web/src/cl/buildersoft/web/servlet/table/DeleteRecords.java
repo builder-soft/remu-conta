546109f2aee78d655eb9e6873b89fc5c6b43887b
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-03-15 16:17:11 +0000

    Version inicial del servlet, no hace nada, solo redirige donde corresponde.

---
+++ app-web/src/cl/buildersoft/web/servlet/table/DeleteRecords.java
@@ -0,0 +1,32 @@
+package cl.buildersoft.web.servlet.table;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/DeleteRecords")
+public class DeleteRecords extends HttpServlet {
+
+	private static final long serialVersionUID = -2340853411641380529L;
+
+	public DeleteRecords() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/common/no-access.jsp")
+				.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		request.getRequestDispatcher("/admin/LoadTable").forward(request,
+				response);
+	}
+
+}
