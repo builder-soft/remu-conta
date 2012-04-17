602b2f5ce0bf6bf8ce7a157622bbf88baca443be
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-04-17 18:06:20 +0000

    Cambio para optimizar el desarrollo, ejemplo en la clase EnterpriseManager

--- remcon-web/src/cl/buildersoft/web/servlet/BSHttpServlet.java
+++ remcon-web/src/cl/buildersoft/web/servlet/BSHttpServlet.java
@@ -1,6 +1,7 @@
 package cl.buildersoft.web.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,14 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import cl.buildersoft.framework.beans.BSHeadConfig;
 import cl.buildersoft.framework.beans.BSTableConfig;
+import cl.buildersoft.framework.database.BSmySQL;
 
 public abstract class BSHttpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected abstract BSTableConfig getBSTableConfig(HttpServletRequest request);
-
 
 	public BSHttpServlet() {
 		super();
@@ -24,7 +24,7 @@ public abstract class BSHttpServlet extends HttpServlet {
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-//		BSHeadConfig head = getBSHeadConfig();
+		// BSHeadConfig head = getBSHeadConfig();
 		BSTableConfig table = getBSTableConfig(request);
 
 		String uri = request.getRequestURI().substring(
@@ -35,11 +35,26 @@ public abstract class BSHttpServlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		synchronized (session) {
 			session.setAttribute("BSTable", table);
-//			session.setAttribute("BSHead", head);
+			// session.setAttribute("BSHead", head);
 		}
 
 		request.getRequestDispatcher("/servlet/table/LoadTable").forward(
 				request, response);
 	}
 
+	protected BSTableConfig initTable(HttpServletRequest request,
+			String database, String tableName) {
+		BSTableConfig table = new BSTableConfig(database, tableName);
+		BSmySQL mysql = new BSmySQL();
+		Connection conn = mysql.getConnection(request.getServletContext(),
+				database);
+		table.configFields(conn, mysql);
+		return table;
+	}
+
+	protected void hideFields(BSTableConfig table, String... hideFields) {
+		for (String fieldName : hideFields) {
+			table.getField(fieldName).setVisible(false);
+		}
+	}
 }
---
+++ remcon-web/src/cl/buildersoft/web/servlet/remu/EnterpriseManager.java
@@ -0,0 +1,66 @@
+package cl.buildersoft.web.servlet.remu;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import cl.buildersoft.framework.beans.BSTableConfig;
+import cl.buildersoft.web.servlet.BSHttpServlet;
+
+/**
+ * Servlet implementation class EmployeeManager
+ */
+@WebServlet("/servlet/remu/EnterpriseManager")
+public class EnterpriseManager extends BSHttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected BSTableConfig getBSTableConfig(HttpServletRequest request) {
+		BSTableConfig table = initTable(request, "remu", "tEnterprise");
+
+		table.setTitle("Empresas");
+
+		table.getField("cName").setLabel("Nombre/Razón Social");
+		table.getField("cLegalRep").setLabel("Representante Legal");
+		table.getField("cCategory").setLabel("Giro");
+		table.getField("cAddress").setLabel("Dirección");
+		table.getField("cPhone").setLabel("Teléfono");
+		table.getField("cRutLegalRep").setLabel("Rut representante Legal");
+		table.getField("cMutualFactor").setLabel("Factor Mutual");
+		table.getField("cCompensationFund").setLabel("Caja de compensación");
+		
+		
+
+		hideFields(table, "cRutLegalRep", "cMutual", "cMutualFactor",
+				"cCompensationFund");
+
+		/**
+		 * table.getField("cLastName1").setLabel("A. Paterno");
+		 * 
+		 * table.getField("cLastName2").setLabel("A. Materno");
+		 * table.getField("cName").setLabel("Nombre");
+		 * table.getField("cBirthDate").setLabel("Nacimiento");
+		 * table.getField("cAddress").setLabel("Dirección");
+		 * table.getField("cGenere").setLabel("Género");
+		 * table.getField("cCountry").setLabel("Nacionalidad");
+		 * table.getField("cMaritalStatus").setLabel("Estado Civil");
+		 * 
+		 * String[] noVisibleFields = { "cEmail", "cMovil", "cPhone", "cComuna",
+		 * "cAddress", "cBirthDate" }; for (String fieldName : noVisibleFields)
+		 * { table.getField(fieldName).setVisible(false); }
+		 * 
+		 * BSAction previtionalInformation = new BSAction("PREVITIONAL",
+		 * BSActionType.Record);
+		 * previtionalInformation.setLabel("Información Previsional");
+		 * table.addAction(previtionalInformation);
+		 * 
+		 * BSAction contractualInformation = new BSAction("CONTRACTUAL",
+		 * BSActionType.Record);
+		 * contractualInformation.setLabel("Información Contractual");
+		 * table.addAction(contractualInformation);
+		 * 
+		 * BSAction payMode = new BSAction("PAY_MODE", BSActionType.Record);
+		 * payMode.setLabel("Modo de Pago"); table.addAction(payMode);
+		 */
+		return table;
+	}
+}
