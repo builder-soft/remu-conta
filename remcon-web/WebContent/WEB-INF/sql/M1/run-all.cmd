4217dbf0df6614f0bd792eeeedbc4c7f5fc9d5d3
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-04-18 19:29:40 +0000

    Se renombran los archivos para mejorar la lectura

--- remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
+++ remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
@@ -12,11 +12,17 @@ cls
 mysql -u root -padmin < delete-tables.sql.txt
 mysql -u root -padmin < create-bsframework.sql.txt
 mysql -u root -padmin < create-bscommon.sql.txt
+mysql -u root -padmin < create-remu.sql.txt
 
-for %%i in (create-db*.sql.txt) do mysql -u root -padmin < %%i
-for %%i in (create-rules*.sql.txt) do mysql -u root -padmin < %%i
-for %%i in (create-sp*.sql.txt) do mysql -u root -padmin < %%i
-for %%i in (create-data*.sql.txt) do mysql -u root -padmin < %%i
+rem mysql -u root -padmin < data-menu.sql.txt
 
-mysql -u root -padmin < create-menu.sql.txt
+mysql -u root -padmin < rules-bscommon.sql.txt
+mysql -u root -padmin < rules-bsframework.sql.txt
+mysql -u root -padmin < rules-remu.sql.txt
+
+rem for %%i in (create-db*.sql.txt) do mysql -u root -padmin < %%i
+rem for %%i in (create-rules*.sql.txt) do mysql -u root -padmin < %%i
+for %%i in (sp*.sql.txt) do mysql -u root -padmin < %%i
+for %%i in (data*.sql.txt) do mysql -u root -padmin < %%i
+
 
