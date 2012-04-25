b919bb16bc07facad17a3cb5b385201f872a8845
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-04-25 13:50:00 +0000

    Se aplican modificaciones en las tablas existentes

--- remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
+++ remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
@@ -11,18 +11,15 @@ rem cargar datos para prueba
 cls
 mysql -u root -padmin < erase-tables.sql.txt
 mysql -u root -padmin < create-bsframework.sql.txt
-rem mysql -u root -padmin < create-bscommon.sql.txt
 mysql -u root -padmin < create-remcon.sql.txt
 
-rem mysql -u root -padmin < rules-bscommon.sql.txt
 mysql -u root -padmin < rules-bsframework.sql.txt
 mysql -u root -padmin < rules-remcon.sql.txt
 
-rem for %%i in (data-*.sql.txt) do mysql -u root -padmin < %%i
 mysql -u root -padmin < data-bsframework.sql.txt
 mysql -u root -padmin < data-remcon.sql.txt
 mysql -u root -padmin < data-menu.sql.txt
 
 for %%i in (sp-*.sql.txt) do mysql -u root -t -padmin < %%i
 
-
+mysql -u root -t -padmin < testSP.sql.txt
--- remcon-web/WebContent/WEB-INF/sql/M1/run-sp.cmd
+++ remcon-web/WebContent/WEB-INF/sql/M1/run-sp.cmd
@@ -2,4 +2,4 @@ cls
 @echo off
 for %%i in (sp-*.sql.txt) do mysql -u root -t -padmin < %%i
 
-
+mysql -u root -t -padmin < testSP.sql.txt
