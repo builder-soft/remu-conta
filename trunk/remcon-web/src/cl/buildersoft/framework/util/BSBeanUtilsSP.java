e35e14a44c65b0f6894b0a295ed6a3062fcdd480
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-05-09 03:06:30 +0000

    Se crea la clase BSBeanUtilsSP extends BSBeanUtils

---
+++ trunk/remcon-web/src/cl/buildersoft/framework/util/BSBeanUtilsSP.java
@@ -0,0 +1,40 @@
+package cl.buildersoft.framework.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.buildersoft.framework.beans.BSBean;
+import cl.buildersoft.framework.database.BSBeanUtils;
+import cl.buildersoft.framework.database.BSmySQL;
+import cl.buildersoft.framework.exception.BSDataBaseException;
+import cl.buildersoft.framework.exception.BSProgrammerException;
+
+public class BSBeanUtilsSP extends BSBeanUtils {
+	public List<BSBean> list(Connection conn, BSBean bean, String spName,
+			List<Object> params) {
+
+		BSmySQL mysql = new BSmySQL();
+		ResultSet rs = mysql.callSingleSP(conn, spName, params);
+
+		List<BSBean> out = new ArrayList<BSBean>();
+		Long id = null;
+		try {
+			while (rs.next()) {
+				id = rs.getLong(1);
+				BSBean object = bean.getClass().newInstance();
+				object.setId(id);
+				super.search(conn, object);
+				out.add((BSBean) object);
+			}
+		} catch (SQLException e) {
+			throw new BSDataBaseException("", e.getMessage());
+		} catch (Exception e) {
+			throw new BSProgrammerException("", e.getMessage());
+		}
+
+		return out;
+	}
+}
