322b0e34e8f4fdff7bf8e2194a3711100c5273ed
damoreno <damoreno@gmail.com>
2012-05-11 16:43:49 +0000

    Actualizacion de la claseEmployeeInformation

--- remcon-web/src/cl/buildersoft/web/servlet/table/EmployeeInformation.java
+++ remcon-web/src/cl/buildersoft/web/servlet/table/EmployeeInformation.java
@@ -14,11 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import cl.buildersoft.framework.beans.Account;
+import cl.buildersoft.framework.beans.Agreement;
 import cl.buildersoft.framework.beans.BSTableConfig;
 import cl.buildersoft.framework.beans.Employee;
 import cl.buildersoft.framework.database.BSmySQL;
 import cl.buildersoft.framework.exception.BSDataBaseException;
 import cl.buildersoft.framework.util.BSBeanUtilsSP;
+import cl.buildersoft.web.servlet.config.employee.AgreementService;
+import cl.buildersoft.web.servlet.config.employee.AgreementServiceImpl;
 
 /**
  * Servlet implementation class EditRecord
@@ -34,18 +37,11 @@ public class EmployeeInformation extends AbstractServletUtil {
 
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		BSTableConfig table = null;
 		Long id = Long.parseLong(request.getParameter("cId"));
-		synchronized (session) {
-			table = (BSTableConfig) session.getAttribute("BSTable");
-		}
-
 		Connection conn = null;
 		BSmySQL mySQL = new BSmySQL();
 
 		conn = mySQL.getConnection(request);
-		Employee employee = getEmployee(conn, new BSBeanUtilsSP(), id);
 				
 		BSmySQL mysql = new BSmySQL();
 		ResultSet rsApv = mysql.callSingleSP(conn, "pListBoardByType","APV");
@@ -58,9 +54,6 @@ public class EmployeeInformation extends AbstractServletUtil {
 				bsApv.setKey(rsApv.getString("cKey"));
 				bsApv.setValue(rsApv.getString("cName"));
 				listadoApv.add(bsApv);
-				//bu.search(conn, domainAttribute);
-
-				//out.put(domainAttribute.getKey(), domainAttribute);
 			}
 			mysql.closeSQL(rsApv);
 		} catch (SQLException e) {
@@ -77,9 +70,6 @@ public class EmployeeInformation extends AbstractServletUtil {
 				bsAfp.setKey(rsAfp.getString("cKey"));
 				bsAfp.setValue(rsAfp.getString("cName"));
 				listadoAfp.add(bsAfp);
-				//bu.search(conn, domainAttribute);
-
-				//out.put(domainAttribute.getKey(), domainAttribute);
 			}
 			mysql.closeSQL(rsAfp);
 		} catch (SQLException e) {
@@ -104,37 +94,11 @@ public class EmployeeInformation extends AbstractServletUtil {
 				bsApvEmp.setcIdCurrency(rsApvForEmp.getLong("cCurrency"));
 				bsApvEmp.setcAmount(rsApvForEmp.getLong("cAmount"));
 				listadoApvEmp.add(bsApvEmp);
-				//bu.search(conn, domainAttribute);
-
-				//out.put(domainAttribute.getKey(), domainAttribute);
 			}
 			mysql.closeSQL(rsApvForEmp);
 		} catch (SQLException e) {
 			throw new BSDataBaseException("1000", e.getMessage());
 		}	
-
-		List<Object> parameterEmp = new ArrayList<Object>();
-		parameterEmp.add(id);
-		parameterEmp.add("PFM");
-		ResultSet rsAfpForEmp = mysql.callSingleSP(conn, "pListAccountsForEmployeeAndType",parameterEmp);
-		
-		Account bsAfpEmp =  null;
-		try {
-			while (rsAfpForEmp.next()) {
-				bsAfpEmp = new Account();
-				bsAfpEmp.setId(rsAfpForEmp.getLong("cId"));
-				bsAfpEmp.setKey(rsAfpForEmp.getString("cKeyInstitution"));
-				bsAfpEmp.setValue(rsAfpForEmp.getString("cNameInstitution"));
-				bsAfpEmp.setcKeyCurrency(rsAfpForEmp.getString("cKeyCurrency"));
-				bsAfpEmp.setcIdCurrency(rsAfpForEmp.getLong("cCurrency"));
-				bsAfpEmp.setcAmount(rsAfpForEmp.getLong("cAmount"));
-			}
-			mysql.closeSQL(rsApvForEmp);
-		} catch (SQLException e) {
-			throw new BSDataBaseException("1000", e.getMessage());
-		}
-		
-		
 		
 		ResultSet rsCurrency = mysql.callSingleSP(conn, "pListBoardByType","CURRENCY");
 		Account bsCurrency =  null;
@@ -146,9 +110,6 @@ public class EmployeeInformation extends AbstractServletUtil {
 				bsCurrency.setKey(rsCurrency.getString("cKey"));
 				bsCurrency.setValue(rsCurrency.getString("cName"));
 				listadoCurrency.add(bsCurrency);
-				//bu.search(conn, domainAttribute);
-
-				//out.put(domainAttribute.getKey(), domainAttribute);
 			}
 			mysql.closeSQL(rsCurrency);
 		} catch (SQLException e) {
@@ -170,23 +131,6 @@ public class EmployeeInformation extends AbstractServletUtil {
 		} catch (SQLException e) {
 			throw new BSDataBaseException("1000", e.getMessage());
 		}		
-
-		List<Object> parameterExboxEmp = new ArrayList<Object>();
-		parameterExboxEmp.add(id);
-		parameterExboxEmp.add("EX_BOX");		
-		ResultSet rsExBoxEmp = mysql.callSingleSP(conn, "pListAccountsForEmployeeAndType",parameterExboxEmp);
-		Account exBoxEmp = new Account();
-		try {
-			while (rsExBoxEmp.next()) {
-				exBoxEmp = new Account();
-				exBoxEmp.setId(rsExBoxEmp.getLong("cId"));
-				exBoxEmp.setKey(rsExBoxEmp.getString("cKey"));
-				exBoxEmp.setValue(rsExBoxEmp.getString("cName"));
-			}
-			mysql.closeSQL(rsExBoxEmp);
-		} catch (SQLException e) {
-			throw new BSDataBaseException("1000", e.getMessage());
-		}
 		
 		ResultSet rsHealth = mysql.callSingleSP(conn, "pListBoardByType","HEALTH");
 		Account bsHealth =  null;
@@ -203,23 +147,8 @@ public class EmployeeInformation extends AbstractServletUtil {
 		} catch (SQLException e) {
 			throw new BSDataBaseException("1000", e.getMessage());
 		}		
-
-		List<Object> parameterHealthEmp = new ArrayList<Object>();
-		parameterHealthEmp.add(id);
-		parameterHealthEmp.add("HEALTH");		
-		ResultSet rsHealthEmp = mysql.callSingleSP(conn, "pListAccountsForEmployeeAndType",parameterHealthEmp);
-		Account healthEmp = new Account();
-		try {
-			while (rsHealthEmp.next()) {
-				healthEmp = new Account();
-				healthEmp.setId(rsHealthEmp.getLong("cId"));
-				healthEmp.setKey(rsHealthEmp.getString("cKey"));
-				healthEmp.setValue(rsHealthEmp.getString("cName"));
-			}
-			mysql.closeSQL(rsHealthEmp);
-		} catch (SQLException e) {
-			throw new BSDataBaseException("1000", e.getMessage());
-		}		
+		
+		Agreement agreementEmp = getAgreement(conn,new BSBeanUtilsSP(),id);
 		
 		request.setAttribute("listadoAfp", listadoAfp);
 		request.setAttribute("listadoApv", listadoApv);
@@ -227,8 +156,7 @@ public class EmployeeInformation extends AbstractServletUtil {
 		request.setAttribute("listadoApvEmp", listadoApvEmp);
 		request.setAttribute("listadoExBox", listadoExBox);	
 		request.setAttribute("listadoHealth", listadoHealth);
-		request.setAttribute("afpEmp", bsAfpEmp);
-		request.setAttribute("healthEmp", healthEmp);
+		request.setAttribute("agreementEmp", agreementEmp);
 
 		request.setAttribute("Action", "Update");
 		request.getRequestDispatcher("/WEB-INF/jsp/table/previtional-information.jsp")
@@ -236,6 +164,14 @@ public class EmployeeInformation extends AbstractServletUtil {
 	}
 
 
+	public Agreement getAgreement(Connection conn, BSBeanUtilsSP bu,
+			Long idEmployee) {
+		AgreementService agreementService = new AgreementServiceImpl();
+		Agreement out = agreementService.getAgreementByEmployee(conn,
+				idEmployee);
+		return out;
+	}
+	
 	private Employee getEmployee(Connection conn, BSBeanUtilsSP bu, Long id) {
 		Employee out = new Employee();
 		out.setId(id);
