1d9903465ea22a121e3bc93e7f3902c8f667d0cd
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-06-03 06:10:10 +0000

    Se calcula Cotizacion obligatoria de AFP (cObligatoryQuote)

--- trunk/remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
+++ trunk/remcon-web/WebContent/WEB-INF/sql/M1/run-all.cmd
@@ -11,7 +11,7 @@ mysql -u root -padmin --default-character-set=utf8 < rules-bsframework.sql.txt
 mysql -u root -padmin --default-character-set=utf8 < rules-remcon.sql.txt
 
 for %%i in (fn-*.sql.txt) do mysql -u root -padmin --default-character-set=utf8 < %%i
-for %%i in (sp-*.sql.txt) do mysql -u root -t -padmin --default-character-set=utf8 < %%i
+for %%i in (sp-*.sql.txt) do mysql -u root -padmin --default-character-set=utf8 < %%i
 
 mysql -u root -padmin --default-character-set=utf8 < data-bsframework.sql.txt
 mysql -u root -padmin --default-character-set=utf8 < data-remcon.sql.txt
