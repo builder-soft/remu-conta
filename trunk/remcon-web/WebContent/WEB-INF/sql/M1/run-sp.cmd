d8b533ec5b5c1957d58950759b697823b666c259
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-07-05 16:26:10 +0000

    Se calcula el ajuste de sencillo.

--- trunk/remcon-web/WebContent/WEB-INF/sql/M1/run-sp.cmd
+++ trunk/remcon-web/WebContent/WEB-INF/sql/M1/run-sp.cmd
@@ -1,4 +1,4 @@
 for %%i in (fn-*.sql.txt) do mysql -u root -t -padmin --default-character-set=utf8 < %%i
 for %%i in (sp-*.sql.txt) do mysql -u root -t -padmin --default-character-set=utf8 < %%i
 
-mysql -u root -padmin --default-character-set=utf8 -t < testSP.sql.txt
+mysql -u root -t -padmin --default-character-set=utf8 < testSP.sql.txt
