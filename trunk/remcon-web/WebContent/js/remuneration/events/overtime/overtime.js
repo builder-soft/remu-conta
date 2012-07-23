6cf665ff01ba506cf4c1144deaf88ce4b9f31dc2
claudio.moscoso <claudio.moscoso@afae724c-4187-47be-f31d-de5aa923d161>
2012-07-23 12:37:14 +0000

    Desarrollo de funcionalidad de Horas Extras.

--- trunk/remcon-web/WebContent/js/remuneration/events/overtime/overtime.js
+++ trunk/remcon-web/WebContent/js/remuneration/events/overtime/overtime.js
@@ -1,96 +1,115 @@
 var selectedRadio = 1;
+var overtimeId = 0;
 var action = "";
-var date = null;
+var day = null;
 var percent = null;
 var amount = null;
 
-function add(lastDayMonth) {
-	$("#defaultButtons").hide();
-	$("#commitButtons").fadeIn(speed);
+function add(lastDayMonth, overtimePercent, periodName) {
+    $("#defaultButtons").hide();
+    $("#commitButtons").fadeIn(speed);
 
-	action = "ADD";
+    action = "ADD";
 
-	// $('#overtimeTable').append("<tr><td class='cDataTD'>my data</td><td
-	// class='cDataTD'>more data</td></tr>");
+    // $('#overtimeTable').append("<tr><td class='cDataTD'>my data</td><td
+    // class='cDataTD'>more data</td></tr>");
 
-	var row = document.getElementById('overtimeTable').insertRow(-1);
+    var row = document.getElementById('overtimeTable').insertRow(-1);
 
-	buidInputs(row, true, lastDayMonth);
+    buidInputs(row, true, lastDayMonth, overtimePercent, periodName);
 
-	loadFormat();
+    loadFormat();
 
 }
 
-function commitAdd() {
-	// $("#commitButtons").hide();
-	// $("#defaultButtons").fadeIn(speed);
-
-	document.getElementById("cDate").value = document.getElementById("date").value;
-	document.getElementById("cPercent").value = document
-			.getElementById("percent").value;
-	document.getElementById("cAmount").value = document
-			.getElementById("amount").value;
-
-	document.getElementById("frm").setAttribute("action",
-			contextPath + "/servlet/remuneration/events/overtime/AddOvertime");
+function commit() {
+    if (action == "ERASE") {
+	document.getElementById("frm")
+		.setAttribute("action", contextPath + "/servlet/remuneration/events/overtime/EraseOvertime");
+	document.getElementById("cOvertime").value = overtimeId;
 	document.getElementById("frm").submit();
+
+    } else {
+	document.getElementById("cDay").value = 7;
+	document.getElementById("cPercent").value = document.getElementById("percent").value;
+	document.getElementById("cAmount").value = document.getElementById("amount").value;
+	if (action == "ADD") {
+	    document.getElementById("frm").setAttribute("action",
+		    contextPath + "/servlet/remuneration/events/overtime/AddOvertime");
+	} else if (action == "EDIT") {
+	    document.getElementById("cOvertime").value = overtimeId;
+	    document.getElementById("frm").setAttribute("action",
+		    contextPath + "/servlet/remuneration/events/overtime/UpdateOvertime");
+	}
+    }
+    document.getElementById("frm").submit();
 }
 
 function cancel() {
-	$("#commitButtons").hide();
-	$("#defaultButtons").fadeIn(speed);
+    $("#commitButtons").hide();
+    $("#defaultButtons").fadeIn(speed);
 
-	if (action == "ADD") {
-		document.getElementById('overtimeTable').deleteRow(-1);
-	}
+    if (action == "ADD") {
+	document.getElementById('overtimeTable').deleteRow(-1);
+    } else if (action == "EDIT") {
+	document.getElementById("frm").setAttribute("action", contextPath + "/servlet/remuneration/events/overtime/OvertimeMain");
+	document.getElementById("frm").submit();
+    }
 }
 
-function selectRadio(index) {
-	selectedRadio = index;
+function selectRadio(index, id) {
+    selectedRadio = index;
+    overtimeId = id;
 }
 
-function editOvertime(lastDayMonth) {
-	$("#defaultButtons").hide();
-	$("#commitButtons").fadeIn(speed);
-	action = "EDIT";
+function editOvertime(lastDayMonth, overtimePercent, periodName) {
+    $("#defaultButtons").hide();
+    $("#commitButtons").fadeIn(speed);
+    action = "EDIT";
 
-	var row = document.getElementById('overtimeTable').rows[selectedRadio];
-//	row.cells[0].innerHTML = "&nbsp;-&nbsp;";
+    var row = document.getElementById('overtimeTable').rows[selectedRadio];
 
-	date = row.cells[1].innerHTML;
-	percent = row.cells[2].innerHTML;
-	amount = row.cells[3].innerHTML;
+    day = 7; // row.cells[1].innerHTML;
+    percent = row.cells[2].innerHTML;
+    amount = row.cells[3].innerHTML;
 
-	buidInputs(row, false, lastDayMonth);
+    buidInputs(row, false, lastDayMonth, overtimePercent, periodName);
 
-	document.getElementById("date").value = date;
-	document.getElementById("percent").value = percent;
-	document.getElementById("amount").value = amount;
+    document.getElementById("day").value = day;
+    document.getElementById("percent").value = percent;
+    document.getElementById("amount").value = amount;
 
-	loadFormat();
+    loadFormat();
 
 }
 
-function buidInputs(row, isNew, lastDayMonth) {
-	var cell = isNew ? row.insertCell(-1) : row.cells[0];	
-	cell.setAttribute("class", "cDataTD");
-	cell.innerHTML = "&nbsp;-&nbsp;";
+function buidInputs(row, isNew, lastDayMonth, overtimePercent, periodName) {
+    var cell = isNew ? row.insertCell(-1) : row.cells[0];
+    cell.setAttribute("class", "cDataTD");
+    cell.innerHTML = "&nbsp;-&nbsp;";
 
-	var cell = isNew ? row.insertCell(-1) : row.cells[1];
-	cell.setAttribute("class", "cDataTD");
-	var html = "<select id='lastDayMonth'>";
-	for(var i = 1; i<=lastDayMonth; i++){
-		html += "<option value='" + i + "'>" + i +"</option>";
-	}
-	html += "</select>";
-	cell.innerHTML = html;
+    var cell = isNew ? row.insertCell(-1) : row.cells[1];
+    cell.setAttribute("class", "cDataTD");
+    var html = "<select id='day'>";
+    for ( var i = 1; i <= lastDayMonth; i++) {
+	html += "<option value='" + i + "'>" + i + "</option>";
+    }
+    html += "</select> de " + periodName + "";
+    cell.innerHTML = html;
 
-	var cell = isNew ? row.insertCell(-1) : row.cells[2];
-	cell.setAttribute("class", "cDataTD");
-	cell.innerHTML = "<input type='text' id='percent'>";
+    var cell = isNew ? row.insertCell(-1) : row.cells[2];
+    cell.setAttribute("class", "cDataTD");
+    cell.innerHTML = "<input type='text' id='percent' value='" + overtimePercent + "'>";
 
-	var cell = isNew ? row.insertCell(-1) : row.cells[3];
-	cell.setAttribute("class", "cDataTD");
-	cell.innerHTML = "<input type='text' id='amount'>";
+    var cell = isNew ? row.insertCell(-1) : row.cells[3];
+    cell.setAttribute("class", "cDataTD");
+    cell.innerHTML = "<input type='text' id='amount' value='1'>";
 
+}
+
+function eraseOvertime() {
+    action = "ERASE";
+    if (confirm('Esta seguro de borrar esta asignacion de horas extras?')) {
+	commit();
+    }
 }
