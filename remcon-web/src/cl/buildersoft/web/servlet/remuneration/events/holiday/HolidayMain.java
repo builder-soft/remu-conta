e17468a32177b344384bd6e5706111e5c5b10b7a
claudio.moscoso <claudio.moscoso@gmail.com>
2012-08-08 04:35:09 +0000

    Se avanza con el desarrollo de calculo de vacaciones.

--- remcon-web/src/cl/buildersoft/web/servlet/remuneration/events/holiday/HolidayMain.java
+++ remcon-web/src/cl/buildersoft/web/servlet/remuneration/events/holiday/HolidayMain.java
@@ -3,6 +3,9 @@ package cl.buildersoft.web.servlet.remuneration.events.holiday;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,14 +32,31 @@ public class HolidayMain extends HttpServlet {
 		Employee employee = getEmployee(conn, request);
 		ResultSet holidays = mysql.callSingleSP(conn, "pListHoliday", employee.getId());
 		Agreement agreement = getAgreement(conn, employee);
+
+		List<Object> params = getParameters(employee, agreement);
+		ResultSet holidayInfo = mysql.callSingleSP(conn, "fGetHolydayInfo", params);
+
 		// List<Holiday> holidays = (List<Holiday>) bu.list(conn, new Holiday(),
 		// "cEmployee=?", employee.getId());
 
 		request.setAttribute("Agreement", agreement);
 		request.setAttribute("Employee", employee);
 		request.setAttribute("Holidays", holidays);
+		request.setAttribute("HolidayInfo", holidayInfo);
 
 		request.getRequestDispatcher("/WEB-INF/jsp/remuneration/events/holiday/holiday.jsp").forward(request, response);
+	}
+
+	private List<Object> getParameters(Employee employee, Agreement agreement) {
+		List<Object> params = new ArrayList<Object>();
+		params.add(employee.getId());
+		params.add(agreement.getStartContract());
+		params.add(getLastDate(agreement));
+		return params;
+	}
+
+	private Object getLastDate(Agreement agreement) {
+		return agreement.getContractType().equals(1L) ? new Date() : agreement.getEndContract();
 	}
 
 	private Employee getEmployee(Connection conn, HttpServletRequest request) {
