82be670c5bc68567df0c7af9a2d592efb8a0bf4e
claudio.moscoso <claudio.moscoso@gmail.com>
2012-08-16 01:52:59 +0000

    Primera version del desarrollo de vacaciones tomadas, ahora haremos el intento de desarrollarlo en una clase Java para bajar la complejidad.

--- remcon-web/src/cl/buildersoft/business/beans/Holiday.java
+++ remcon-web/src/cl/buildersoft/business/beans/Holiday.java
@@ -8,30 +8,47 @@ public class Holiday extends BSBean {
 	private static final long serialVersionUID = 4180166895306245829L;
 	private String TABLE = "tHoliday";
 	private Long employee = null;
+	private Date creation = null;
 	private Date from = null;
 	private Date to = null;
-
+	private Integer normal = null;
+	private Integer progressive = null;
+	
 	public Long getEmployee() {
 		return employee;
 	}
-
 	public void setEmployee(Long employee) {
 		this.employee = employee;
 	}
-
+	public Date getCreation() {
+		return creation;
+	}
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
 	public Date getFrom() {
 		return from;
 	}
-
 	public void setFrom(Date from) {
 		this.from = from;
 	}
-
 	public Date getTo() {
 		return to;
 	}
-
 	public void setTo(Date to) {
 		this.to = to;
 	}
+	public Integer getNormal() {
+		return normal;
+	}
+	public void setNormal(Integer normal) {
+		this.normal = normal;
+	}
+	public Integer getProgressive() {
+		return progressive;
+	}
+	public void setProgressive(Integer progressive) {
+		this.progressive = progressive;
+	}
+ 
 }
