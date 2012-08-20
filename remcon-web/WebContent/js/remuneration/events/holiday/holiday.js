caaaf2115473ba23632888381b88c1f71656e77d
claudio.moscoso <claudio.moscoso@gmail.com>
2012-08-20 02:07:23 +0000

    Se avanza con la validación de la fecha de término de vacaciones.

--- remcon-web/WebContent/js/remuneration/events/holiday/holiday.js
+++ remcon-web/WebContent/js/remuneration/events/holiday/holiday.js
@@ -4,15 +4,20 @@ function acceptHoliday(){
 
 function calculateEndDate(){
 	var url = contextPath + '/servlet/remuneration/events/holiday/ValidateHolidayParameters';
-	var data = {p1:123};
+	var from = document.getElementById("cFrom").value;
+	var normal = document.getElementById("cNormal").value;
+	var creeping = document.getElementById("cCreeping").value;
+	
+	var data = {cFrom:from, cNormal:normal, cCreeping:creeping};
 	$.ajax({url:url,type:"post", data:data, error:error, success:success});
 	
 }
 
-function error(){
-	alert('error');
+function error(jqXHR, textStatus, errorThrown){
+//	alert('Error : ' + textStatus);
+	document.getElementById("dateMessage").innerHTML = jqXHR.responseText; // + " - " + textStatus + " - " + errorThrown;
 }
 
-function success(v){
-	alert('ok ' + v);
+function success(response){
+	document.getElementById("cTo").value = response;
 }
