ba9d0df90f0c2dc0899ed8f1e589f07bef96fcec
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2012-08-29 20:54:59 +0000

    Se complementa el mantenedor genérico con las validaciones de números y fechas.

--- trunk/remcon-web/test/cl/buildersoft/business/test/FormatTest.java
+++ trunk/remcon-web/test/cl/buildersoft/business/test/FormatTest.java
@@ -1,7 +1,7 @@
 package cl.buildersoft.business.test;
 
 import java.text.DateFormat;
-import java.text.DateFormatSymbols;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -12,30 +12,51 @@ public class FormatTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Locale cl = new Locale("en");
+//		Locale cl = new Locale("cl");
+//		Locale cl = new Locale("de");
+		Locale cl = new Locale("es");
+//		Locale cl = Locale.GERMAN; 
+//		Locale cl = Locale.CANADA; 
 
-		
- 
+		// formatDate(cl);
+//		listLocales();
+
+		formatNumber(cl);
+	}
+
+	private static void listLocales() {
+		Locale[] ls = Locale.getAvailableLocales();
+		for (Locale l : ls) {
+			System.out.println(l.getCountry() + ", " + l.getDisplayCountry() + ", " + l.getDisplayLanguage());
+		}
+	}
+
+	private static void formatDate(Locale cl) {
 		System.out.println(DateFormat.getDateInstance(DateFormat.DEFAULT, cl).format(new Date()));
 		System.out.println(DateFormat.getDateInstance(DateFormat.SHORT, cl).format(new Date()));
 		System.out.println(DateFormat.getDateInstance(DateFormat.MEDIUM, cl).format(new Date()));
 		System.out.println(DateFormat.getDateInstance(DateFormat.LONG, cl).format(new Date()));
-
-		 formatNumber(cl);
 	}
 
 	private static void formatNumber(Locale cl) {
 		try {
-			Double n = 1234567890.123456789;
+			Double n1 = 1234567890.123456789;
+			Double n2 = 0.12;
+			Double n3 = -41000.12;
+			Double n4 = 0D;
 
-//			Locale[] ls = Locale.getAvailableLocales();
-//			for (Locale l : ls) {
-//				System.out.println(l.getCountry() + ", " + l.getDisplayCountry() + ", " + l.getDisplayLanguage());
-//			}
+//			NumberFormat format = NumberFormat.getNumberInstance(cl);
+			DecimalFormat fmt = (DecimalFormat) NumberFormat.getNumberInstance(cl);
+//			fmt.applyLocalizedPattern("###,###,##0.0###");
+			fmt.applyLocalizedPattern("###.###.##0,#####");
 
-			NumberFormat format = NumberFormat.getNumberInstance(cl);
+//			System.out.println(fmt.format(n1));
+//			System.out.println(fmt.format(n2));
+			System.out.println("CL " + fmt.format(n1));
+			System.out.println("CL " + fmt.format(n2));
+			System.out.println("CL " + fmt.format(n3));
+			System.out.println("CL " + fmt.format(n4));
 
-			System.out.println(format.format(n));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
