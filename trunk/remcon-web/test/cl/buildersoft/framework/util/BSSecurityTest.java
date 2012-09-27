95e695fddfb431dfb690b4483b3a47567018fd94
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2012-09-27 21:35:42 +0000

    Clases para encriptación de 3des

---
+++ trunk/remcon-web/test/cl/buildersoft/framework/util/BSSecurityTest.java
@@ -0,0 +1,27 @@
+package cl.buildersoft.framework.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BSSecurityTest {
+	String crypted = "+yd80m0xf88TYCB6/hEwUQ==";
+	String clear = "Claudio Moscoso";
+
+	@Test
+	public void testEncript3des() {
+		BSSecurity s = new BSSecurity();
+		crypted = s.encript3des(clear);
+
+		assertTrue(crypted.equals(crypted));
+	}
+
+	@Test
+	public void testDecript3des() {
+		BSSecurity s = new BSSecurity();
+		String response = s.decript3des(crypted);
+
+		assertTrue(response.equals(clear));
+	}
+
+}
