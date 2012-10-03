7ed5edec042cdad225049911c2df6d3eb5411342
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-03 03:07:51 +0000

    Se crean los primeros scripts para contabilidad.

---
+++ trunk/remcon-web/WebContent/WEB-INF/sql/M2/run-all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < erase-tables.sql.txt
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < create-conta.sql.txt
+
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < rules-conta.sql.txt
+	
+	
+##	for i in $(ls fn*.sql.txt); do echo "Running $i..."; mysql -D$1 -u root -padmin < $i; done
+##	for i in $(ls sp*.sql.txt); do echo "Running $i..."; mysql -D$1 -u root -padmin < $i; done
+	
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-menu.sql.txt
+
+#	mysql -D$1 -t -u root -padmin --default-character-set=utf8 -t < testSP.sql.txt
+else
+	echo "Error, falta indicar la base de datos por parámetro..."
+fi
+
