b2fbfd35087f487e9c1fc4bd05123beaaaf7b4ae
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-04 00:03:12 +0000

    Primeras definiciones de cuentas y voucher

--- trunk/remcon-web/WebContent/WEB-INF/sql/M2/run-all.sh
+++ trunk/remcon-web/WebContent/WEB-INF/sql/M2/run-all.sh
@@ -11,6 +11,7 @@ if [ -n "$1" ]; then
 ##	for i in $(ls sp*.sql.txt); do echo "Running $i..."; mysql -D$1 -u root -padmin < $i; done
 	
 	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-menu.sql.txt
+	mysql -D$1 -t -u root -padmin --default-character-set=utf8 < data-conta.sql.txt
 
 #	mysql -D$1 -t -u root -padmin --default-character-set=utf8 -t < testSP.sql.txt
 else
