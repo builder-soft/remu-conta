2dd58ceeebfc64b78621c8237ca174ea63adaf34
claudio.moscoso <claudio.moscoso@gmail.com>
2012-10-05 18:31:18 +0000

    Se completan los casos de prueba para el Voucher

--- remcon-web/src/cl/buildersoft/business/service/impl/VoucherServiceImpl.java
+++ remcon-web/src/cl/buildersoft/business/service/impl/VoucherServiceImpl.java
@@ -1,6 +1,7 @@
 package cl.buildersoft.business.service.impl;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 import cl.buildersoft.business.beans.Voucher;
@@ -34,10 +35,10 @@ public class VoucherServiceImpl implements VoucherService {
 		Boolean commited = Boolean.FALSE;
 
 		voucher.setId(voucherId);
-		
+
 		Boolean found = bu.search(conn, voucher);
 		if (found) {
-			if (voucher.getVoucherStatus().equals(1L)) {
+			if (isPending(voucher)) {
 				voucher.setVoucherStatus(2L);
 				bu.save(conn, voucher);
 				commited = Boolean.TRUE;
@@ -49,21 +50,46 @@ public class VoucherServiceImpl implements VoucherService {
 	}
 
 	@Override
-	public void delete(Connection conn, Long voucherId) {
-		// TODO Auto-generated method stub
+	public Boolean delete(Connection conn, Long voucherId) {
+		Voucher voucher = new Voucher();
+		BSBeanUtils bu = new BSBeanUtils();
+		Boolean deleted = Boolean.FALSE;
 
+		voucher.setId(voucherId);
+
+		Boolean found = bu.search(conn, voucher);
+		if (found) {
+			if (isPending(voucher)) {
+				bu.delete(conn, voucher);
+				deleted = Boolean.TRUE;
+			}
+		}
+		return found && deleted;
+	}
+
+	private Boolean isPending(Voucher voucher) {
+		return voucher.getVoucherStatus().equals(1L);
+	}
+
+	private Boolean isDone(Voucher voucher) {
+		return voucher.getVoucherStatus().equals(2L);
 	}
 
 	@Override
 	public Voucher get(Connection conn, Long voucherId) {
-		// TODO Auto-generated method stub
-		return null;
+		Voucher voucher = new Voucher();
+		BSBeanUtils bu = new BSBeanUtils();
+		voucher.setId(voucherId);
+		if (!bu.search(conn, voucher)) {
+			voucher = null;
+		}
+		return voucher;
 	}
 
 	@Override
 	public List<VoucherDetail> getDetail(Connection conn, Long voucherId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<VoucherDetail> list = new ArrayList<VoucherDetail>();
+		return list;
 	}
 
 }
