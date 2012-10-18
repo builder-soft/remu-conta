86e104d256b8bb1f08a60041418d93c01d67f6f7
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-18 22:11:38 +0000

    Se avanza con el desarrollo del formulario de voucher.

--- trunk/remcon-web/WebContent/js/conta/voucher/edit-voucher.js
+++ trunk/remcon-web/WebContent/js/conta/voucher/edit-voucher.js
@@ -17,9 +17,9 @@ function saveVoucher() {
 		cache : false,
 		data : data,
 		error : error,
-		success : success
+		success : success,
+		async : false
 	});
-
 }
 
 function error(jqXHR, textStatus, errorThrown) {
@@ -43,43 +43,34 @@ function addNewRow() {
 
 	cell = row.insertCell(-1);
 	cell.innerHTML = getVoucherTypeListAsSelect();
-	cell.style.cssText = "text-align: center";	
+	cell.style.cssText = "text-align: center";
 	cell.className = "cDataTD";
-	
+
 	cell = row.insertCell(-1);
-	cell.innerHTML = "<input type='text' id='cDocumentNumber' " +
-		"onfocus='javascript:integerFocus(this);' " + 
-		"onblur='javascript:integerBlur(this);' " +
-		"size='10px' maxlength='10' value='0'>";
-	cell.style.cssText = "text-align: center";	
+	cell.innerHTML = "<input type='text' id='cDocumentNumber' " + "onfocus='javascript:integerFocus(this);' "
+			+ "onblur='javascript:integerBlur(this);' " + "size='10px' maxlength='10' value='0'>";
+	cell.style.cssText = "text-align: center";
 	cell.className = "cDataTD";
-	
+
 	cell = row.insertCell(-1);
-	cell.innerHTML = "<input type='text' id='cNetAmount' " +
-		"onfocus='javascript:doubleFocus(this);' " + 
-		"onblur='javascript:doubleBlur(this);' " +
-		"size='12px' maxlength='12' value='0' "+
-		"style='text-align:right'>";
+	cell.innerHTML = "<input type='text' id='cNetAmount' " + "onfocus='javascript:doubleFocus(this);' "
+			+ "onblur='javascript:doubleBlur(this);' " + "size='12px' maxlength='12' value='0' " + "style='text-align:right'>";
 	cell.style.cssText = "text-align: right";
 	cell.className = "cDataTD";
 
 	cell = row.insertCell(-1);
-	cell.innerHTML = "<input type='text' id='cTax' " +
-		"onfocus='javascript:doubleFocus(this);' " + 
-		"onblur='javascript:doubleBlur(this);' " +
-		"size='12px' maxlength='12' value='0' " +
-		"style='text-align:right'>";
+	cell.innerHTML = "<input type='text' id='cTax' " + "onfocus='javascript:doubleFocus(this);' "
+			+ "onblur='javascript:doubleBlur(this);' " + "size='12px' maxlength='12' value='0' " + "style='text-align:right'>";
 	cell.style.cssText = "text-align: right";
 	cell.className = "cDataTD";
-	
-	
+
 	cell = row.insertCell(-1);
 	cell.innerHTML = getBusinessAreaListAsSelect();
 	cell.style.cssText = "text-align: left";
 	cell.className = "cDataTD";
 
 	cell = row.insertCell(-1);
-	cell.innerHTML = "&nbsp;";
+	cell.innerHTML = "<select id='cCostCenter'/>";
 	cell.style.cssText = "text-align: left";
 	cell.className = "cDataTD";
 
@@ -87,6 +78,25 @@ function addNewRow() {
 	cell.innerHTML = "&nbsp;";
 	cell.style.cssText = "text-align: left";
 	cell.className = "cDataTD";
+
+	fillCostCenter();
+}
+
+function fillCostCenter() {
+	var businessArea = document.getElementById("cBusinessArea").value;
+	alert(businessArea);
+	
+	
+	
+	var costCenter = document.getElementById("cCostCenter");
+	var option = document.createElement("option");
+	option.text = "Kiwi";
+	try {
+		// for IE earlier than version 8
+		costCenter.add(option, costCenter.options[null]);
+	} catch (e) {
+		costCenter.add(option, null);
+	}
 }
 
 function getVoucherTypeListAsSelect() {
@@ -99,9 +109,9 @@ function getVoucherTypeListAsSelect() {
 	return out;
 }
 
-function getBusinessAreaListAsSelect(){
+function getBusinessAreaListAsSelect() {
 	var out = "<select id='cBusinessArea'>";
-	for (var index in businessAreaList) {
+	for ( var index in businessAreaList) {
 		out += "<option value='" + businessAreaList[index].id + "'>" + businessAreaList[index].name + "</option>";
 	}
 
