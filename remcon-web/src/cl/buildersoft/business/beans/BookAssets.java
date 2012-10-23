b7e5340826b82d045f8f0dc68f700025c1873e97
paranguiz.t <paranguiz.t@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-23 15:57:37 +0000

    Bean para tBookAssets

---
+++ remcon-web/src/cl/buildersoft/business/beans/BookAssets.java
@@ -0,0 +1,251 @@
+package cl.buildersoft.business.beans;
+
+import cl.buildersoft.framework.beans.BSBean;
+
+public class BookAssets extends BSBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5820981516543583262L;
+	private String TABLE="tBookAssets";
+	private String book = null;
+	private String gratificationtype = null;
+	private String gratificationamount = null;
+	private String overtimeamount = null;
+	private String participation = null;
+	private String b01 = null;
+	private String b02 = null;
+	private String b03 = null;
+	private String b04 = null;
+	private String b05 = null;
+	private String b06 = null;
+	private String b07 = null;
+	private String b08 = null;
+	private String b09 = null;
+	private String b10 = null;
+	private String extrapay = null;
+	private String totalincometaxable = null;
+	private String income = null;
+	private String familyassignmentstretch = null;
+	private String familyassignmentcount = null;
+	private String familyassignmentamount = null;
+	private String familyretroactive = null;
+	private String simpleloads = null;
+	private String disabilityburdens = null;
+	private String maternalloads = null;
+	private String feeding = null;
+	private String mobilization = null;
+	private String bounty = null;
+	private String monthnotification = null;
+	private String ias = null;
+	private String proportionalholiday = null;
+	private String totalincomenottaxable = null;
+	private String totalincome = null;
+	private String voluntaryindenmization = null;
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	public String getGratificationtype() {
+		return gratificationtype;
+	}
+	public void setGratificationtype(String gratificationtype) {
+		this.gratificationtype = gratificationtype;
+	}
+	public String getGratificationamount() {
+		return gratificationamount;
+	}
+	public void setGratificationamount(String gratificationamount) {
+		this.gratificationamount = gratificationamount;
+	}
+	public String getOvertimeamount() {
+		return overtimeamount;
+	}
+	public void setOvertimeamount(String overtimeamount) {
+		this.overtimeamount = overtimeamount;
+	}
+	public String getParticipation() {
+		return participation;
+	}
+	public void setParticipation(String participation) {
+		this.participation = participation;
+	}
+	public String getB01() {
+		return b01;
+	}
+	public void setB01(String b01) {
+		this.b01 = b01;
+	}
+	public String getB02() {
+		return b02;
+	}
+	public void setB02(String b02) {
+		this.b02 = b02;
+	}
+	public String getB03() {
+		return b03;
+	}
+	public void setB03(String b03) {
+		this.b03 = b03;
+	}
+	public String getB04() {
+		return b04;
+	}
+	public void setB04(String b04) {
+		this.b04 = b04;
+	}
+	public String getB05() {
+		return b05;
+	}
+	public void setB05(String b05) {
+		this.b05 = b05;
+	}
+	public String getB06() {
+		return b06;
+	}
+	public void setB06(String b06) {
+		this.b06 = b06;
+	}
+	public String getB07() {
+		return b07;
+	}
+	public void setB07(String b07) {
+		this.b07 = b07;
+	}
+	public String getB08() {
+		return b08;
+	}
+	public void setB08(String b08) {
+		this.b08 = b08;
+	}
+	public String getB09() {
+		return b09;
+	}
+	public void setB09(String b09) {
+		this.b09 = b09;
+	}
+	public String getB10() {
+		return b10;
+	}
+	public void setB10(String b10) {
+		this.b10 = b10;
+	}
+	public String getExtrapay() {
+		return extrapay;
+	}
+	public void setExtrapay(String extrapay) {
+		this.extrapay = extrapay;
+	}
+	public String getTotalincometaxable() {
+		return totalincometaxable;
+	}
+	public void setTotalincometaxable(String totalincometaxable) {
+		this.totalincometaxable = totalincometaxable;
+	}
+	public String getIncome() {
+		return income;
+	}
+	public void setIncome(String income) {
+		this.income = income;
+	}
+	public String getFamilyassignmentstretch() {
+		return familyassignmentstretch;
+	}
+	public void setFamilyassignmentstretch(String familyassignmentstretch) {
+		this.familyassignmentstretch = familyassignmentstretch;
+	}
+	public String getFamilyassignmentcount() {
+		return familyassignmentcount;
+	}
+	public void setFamilyassignmentcount(String familyassignmentcount) {
+		this.familyassignmentcount = familyassignmentcount;
+	}
+	public String getFamilyassignmentamount() {
+		return familyassignmentamount;
+	}
+	public void setFamilyassignmentamount(String familyassignmentamount) {
+		this.familyassignmentamount = familyassignmentamount;
+	}
+	public String getFamilyretroactive() {
+		return familyretroactive;
+	}
+	public void setFamilyretroactive(String familyretroactive) {
+		this.familyretroactive = familyretroactive;
+	}
+	public String getSimpleloads() {
+		return simpleloads;
+	}
+	public void setSimpleloads(String simpleloads) {
+		this.simpleloads = simpleloads;
+	}
+	public String getDisabilityburdens() {
+		return disabilityburdens;
+	}
+	public void setDisabilityburdens(String disabilityburdens) {
+		this.disabilityburdens = disabilityburdens;
+	}
+	public String getMaternalloads() {
+		return maternalloads;
+	}
+	public void setMaternalloads(String maternalloads) {
+		this.maternalloads = maternalloads;
+	}
+	public String getFeeding() {
+		return feeding;
+	}
+	public void setFeeding(String feeding) {
+		this.feeding = feeding;
+	}
+	public String getMobilization() {
+		return mobilization;
+	}
+	public void setMobilization(String mobilization) {
+		this.mobilization = mobilization;
+	}
+	public String getBounty() {
+		return bounty;
+	}
+	public void setBounty(String bounty) {
+		this.bounty = bounty;
+	}
+	public String getMonthnotification() {
+		return monthnotification;
+	}
+	public void setMonthnotification(String monthnotification) {
+		this.monthnotification = monthnotification;
+	}
+	public String getIas() {
+		return ias;
+	}
+	public void setIas(String ias) {
+		this.ias = ias;
+	}
+	public String getProportionalholiday() {
+		return proportionalholiday;
+	}
+	public void setProportionalholiday(String proportionalholiday) {
+		this.proportionalholiday = proportionalholiday;
+	}
+	public String getTotalincomenottaxable() {
+		return totalincomenottaxable;
+	}
+	public void setTotalincomenottaxable(String totalincomenottaxable) {
+		this.totalincomenottaxable = totalincomenottaxable;
+	}
+	public String getTotalincome() {
+		return totalincome;
+	}
+	public void setTotalincome(String totalincome) {
+		this.totalincome = totalincome;
+	}
+	public String getVoluntaryindenmization() {
+		return voluntaryindenmization;
+	}
+	public void setVoluntaryindenmization(String voluntaryindenmization) {
+		this.voluntaryindenmization = voluntaryindenmization;
+	}
+
+}
