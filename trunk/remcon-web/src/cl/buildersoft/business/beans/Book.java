2029c00248ca82662ab7f30f7b447e304d62324e
paranguiz.t <paranguiz.t@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-23 15:49:45 +0000

    Bean para tBook

---
+++ trunk/remcon-web/src/cl/buildersoft/business/beans/Book.java
@@ -0,0 +1,147 @@
+package cl.buildersoft.business.beans;
+
+import cl.buildersoft.framework.beans.BSBean;
+
+public class Book extends BSBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -745312348098408563L;
+	private String TABLE="tBook";
+	private String period = null;
+	private String employee = null;
+	private String horary = null;
+	private String workeddays = null;
+	private String salaryroot = null;
+	private String overtime = null;
+	private String salaryreceived = null;
+	private String insurancefactoremployee = null;
+	private String insurancefactorenterprise = null;
+	private String contracttype = null;
+	private String netpaymentscope = null;
+	private String topayemployee = null;
+	private String insuranceamountenterprise = null;
+	private String mutualfactor = null;
+	private String paytomutulenterprise = null;
+	private String familyassignmententerprise = null;
+	private String costenterprise = null;
+	private String pensionary = null;
+	private String simpleadjustment = null;
+	
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public String getEmployee() {
+		return employee;
+	}
+	public void setEmployee(String employee) {
+		this.employee = employee;
+	}
+	public String getHorary() {
+		return horary;
+	}
+	public void setHorary(String horary) {
+		this.horary = horary;
+	}
+	public String getWorkeddays() {
+		return workeddays;
+	}
+	public void setWorkeddays(String workeddays) {
+		this.workeddays = workeddays;
+	}
+	public String getSalaryroot() {
+		return salaryroot;
+	}
+	public void setSalaryroot(String salaryroot) {
+		this.salaryroot = salaryroot;
+	}
+	public String getOvertime() {
+		return overtime;
+	}
+	public void setOvertime(String overtime) {
+		this.overtime = overtime;
+	}
+	public String getSalaryreceived() {
+		return salaryreceived;
+	}
+	public void setSalaryreceived(String salaryreceived) {
+		this.salaryreceived = salaryreceived;
+	}
+	public String getInsurancefactoremployee() {
+		return insurancefactoremployee;
+	}
+	public void setInsurancefactoremployee(String insurancefactoremployee) {
+		this.insurancefactoremployee = insurancefactoremployee;
+	}
+	public String getInsurancefactorenterprise() {
+		return insurancefactorenterprise;
+	}
+	public void setInsurancefactorenterprise(String insurancefactorenterprise) {
+		this.insurancefactorenterprise = insurancefactorenterprise;
+	}
+	public String getContracttype() {
+		return contracttype;
+	}
+	public void setContracttype(String contracttype) {
+		this.contracttype = contracttype;
+	}
+	public String getNetpaymentscope() {
+		return netpaymentscope;
+	}
+	public void setNetpaymentscope(String netpaymentscope) {
+		this.netpaymentscope = netpaymentscope;
+	}
+	public String getTopayemployee() {
+		return topayemployee;
+	}
+	public void setTopayemployee(String topayemployee) {
+		this.topayemployee = topayemployee;
+	}
+	public String getInsuranceamountenterprise() {
+		return insuranceamountenterprise;
+	}
+	public void setInsuranceamountenterprise(String insuranceamountenterprise) {
+		this.insuranceamountenterprise = insuranceamountenterprise;
+	}
+	public String getMutualfactor() {
+		return mutualfactor;
+	}
+	public void setMutualfactor(String mutualfactor) {
+		this.mutualfactor = mutualfactor;
+	}
+	public String getPaytomutulenterprise() {
+		return paytomutulenterprise;
+	}
+	public void setPaytomutulenterprise(String paytomutulenterprise) {
+		this.paytomutulenterprise = paytomutulenterprise;
+	}
+	public String getFamilyassignmententerprise() {
+		return familyassignmententerprise;
+	}
+	public void setFamilyassignmententerprise(String familyassignmententerprise) {
+		this.familyassignmententerprise = familyassignmententerprise;
+	}
+	public String getCostenterprise() {
+		return costenterprise;
+	}
+	public void setCostenterprise(String costenterprise) {
+		this.costenterprise = costenterprise;
+	}
+	public String getPensionary() {
+		return pensionary;
+	}
+	public void setPensionary(String pensionary) {
+		this.pensionary = pensionary;
+	}
+	public String getSimpleadjustment() {
+		return simpleadjustment;
+	}
+	public void setSimpleadjustment(String simpleadjustment) {
+		this.simpleadjustment = simpleadjustment;
+	}
+
+}
