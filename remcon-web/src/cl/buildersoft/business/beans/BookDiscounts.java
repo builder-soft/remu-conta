c26fc0c85cb143c568a2449597f54a9459ca3174
paranguiz.t <paranguiz.t@afae724c-4187-47be-f31d-de5aa923d161>
2012-10-23 16:04:32 +0000

    Bean para tBookDiscounts

---
+++ remcon-web/src/cl/buildersoft/business/beans/BookDiscounts.java
@@ -0,0 +1,253 @@
+package cl.buildersoft.business.beans;
+
+import cl.buildersoft.framework.beans.BSBean;
+
+public class BookDiscounts extends BSBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3030891344557483929L;
+	private String TABLE="tBookDiscounts";
+	private String book = null;
+	private String pfmhistory = null;
+	private String exboxsystem = null;
+	private String obligatoryquote = null;
+	private String apvamount = null;
+	private String account2 = null;
+	private String limittaxableforecast = null;
+	private String limittaxabledismissinsurance = null;
+	private String healthhistory = null;
+	private String healthcurrency = null;
+	private String healthamount = null;
+	private String healthclp = null;
+	private String additionalhealth = null;
+	private String healthlegalquote = null;
+	private String healthquote = null;
+	private String additionalpfmcurrency = null;
+	private String additionalpfmamount = null;
+	private String additionalpfmclp = null;
+	private String employerdisabilityinsurance = null;
+	private String unemploymentinsuranceamount = null;
+	private String uniquetax = null;
+	private String advance = null;
+	private String loanenterprise = null;
+	private String d01 = null;
+	private String d02 = null;
+	private String d03 = null;
+	private String d04 = null;
+	private String d05 = null;
+	private String loancompensationfund = null;
+	private String savingcompensationfund = null;
+	private String judicialretention = null;
+	private String subtotallawfuldiscounts = null;
+	private String subtotalotherdiscounts = null;
+	private String totaldiscounts = null;
+
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	public String getPfmhistory() {
+		return pfmhistory;
+	}
+	public void setPfmhistory(String pfmhistory) {
+		this.pfmhistory = pfmhistory;
+	}
+	public String getExboxsystem() {
+		return exboxsystem;
+	}
+	public void setExboxsystem(String exboxsystem) {
+		this.exboxsystem = exboxsystem;
+	}
+	public String getObligatoryquote() {
+		return obligatoryquote;
+	}
+	public void setObligatoryquote(String obligatoryquote) {
+		this.obligatoryquote = obligatoryquote;
+	}
+	public String getApvamount() {
+		return apvamount;
+	}
+	public void setApvamount(String apvamount) {
+		this.apvamount = apvamount;
+	}
+	public String getAccount2() {
+		return account2;
+	}
+	public void setAccount2(String account2) {
+		this.account2 = account2;
+	}
+	public String getLimittaxableforecast() {
+		return limittaxableforecast;
+	}
+	public void setLimittaxableforecast(String limittaxableforecast) {
+		this.limittaxableforecast = limittaxableforecast;
+	}
+	public String getLimittaxabledismissinsurance() {
+		return limittaxabledismissinsurance;
+	}
+	public void setLimittaxabledismissinsurance(String limittaxabledismissinsurance) {
+		this.limittaxabledismissinsurance = limittaxabledismissinsurance;
+	}
+	public String getHealthhistory() {
+		return healthhistory;
+	}
+	public void setHealthhistory(String healthhistory) {
+		this.healthhistory = healthhistory;
+	}
+	public String getHealthcurrency() {
+		return healthcurrency;
+	}
+	public void setHealthcurrency(String healthcurrency) {
+		this.healthcurrency = healthcurrency;
+	}
+	public String getHealthamount() {
+		return healthamount;
+	}
+	public void setHealthamount(String healthamount) {
+		this.healthamount = healthamount;
+	}
+	public String getHealthclp() {
+		return healthclp;
+	}
+	public void setHealthclp(String healthclp) {
+		this.healthclp = healthclp;
+	}
+	public String getAdditionalhealth() {
+		return additionalhealth;
+	}
+	public void setAdditionalhealth(String additionalhealth) {
+		this.additionalhealth = additionalhealth;
+	}
+	public String getHealthlegalquote() {
+		return healthlegalquote;
+	}
+	public void setHealthlegalquote(String healthlegalquote) {
+		this.healthlegalquote = healthlegalquote;
+	}
+	public String getHealthquote() {
+		return healthquote;
+	}
+	public void setHealthquote(String healthquote) {
+		this.healthquote = healthquote;
+	}
+	public String getAdditionalpfmcurrency() {
+		return additionalpfmcurrency;
+	}
+	public void setAdditionalpfmcurrency(String additionalpfmcurrency) {
+		this.additionalpfmcurrency = additionalpfmcurrency;
+	}
+	public String getAdditionalpfmamount() {
+		return additionalpfmamount;
+	}
+	public void setAdditionalpfmamount(String additionalpfmamount) {
+		this.additionalpfmamount = additionalpfmamount;
+	}
+	public String getAdditionalpfmclp() {
+		return additionalpfmclp;
+	}
+	public void setAdditionalpfmclp(String additionalpfmclp) {
+		this.additionalpfmclp = additionalpfmclp;
+	}
+	public String getEmployerdisabilityinsurance() {
+		return employerdisabilityinsurance;
+	}
+	public void setEmployerdisabilityinsurance(String employerdisabilityinsurance) {
+		this.employerdisabilityinsurance = employerdisabilityinsurance;
+	}
+	public String getUnemploymentinsuranceamount() {
+		return unemploymentinsuranceamount;
+	}
+	public void setUnemploymentinsuranceamount(String unemploymentinsuranceamount) {
+		this.unemploymentinsuranceamount = unemploymentinsuranceamount;
+	}
+	public String getUniquetax() {
+		return uniquetax;
+	}
+	public void setUniquetax(String uniquetax) {
+		this.uniquetax = uniquetax;
+	}
+	public String getAdvance() {
+		return advance;
+	}
+	public void setAdvance(String advance) {
+		this.advance = advance;
+	}
+	public String getLoanenterprise() {
+		return loanenterprise;
+	}
+	public void setLoanenterprise(String loanenterprise) {
+		this.loanenterprise = loanenterprise;
+	}
+	public String getD01() {
+		return d01;
+	}
+	public void setD01(String d01) {
+		this.d01 = d01;
+	}
+	public String getD02() {
+		return d02;
+	}
+	public void setD02(String d02) {
+		this.d02 = d02;
+	}
+	public String getD03() {
+		return d03;
+	}
+	public void setD03(String d03) {
+		this.d03 = d03;
+	}
+	public String getD04() {
+		return d04;
+	}
+	public void setD04(String d04) {
+		this.d04 = d04;
+	}
+	public String getD05() {
+		return d05;
+	}
+	public void setD05(String d05) {
+		this.d05 = d05;
+	}
+	public String getLoancompensationfund() {
+		return loancompensationfund;
+	}
+	public void setLoancompensationfund(String loancompensationfund) {
+		this.loancompensationfund = loancompensationfund;
+	}
+	public String getSavingcompensationfund() {
+		return savingcompensationfund;
+	}
+	public void setSavingcompensationfund(String savingcompensationfund) {
+		this.savingcompensationfund = savingcompensationfund;
+	}
+	public String getJudicialretention() {
+		return judicialretention;
+	}
+	public void setJudicialretention(String judicialretention) {
+		this.judicialretention = judicialretention;
+	}
+	public String getSubtotallawfuldiscounts() {
+		return subtotallawfuldiscounts;
+	}
+	public void setSubtotallawfuldiscounts(String subtotallawfuldiscounts) {
+		this.subtotallawfuldiscounts = subtotallawfuldiscounts;
+	}
+	public String getSubtotalotherdiscounts() {
+		return subtotalotherdiscounts;
+	}
+	public void setSubtotalotherdiscounts(String subtotalotherdiscounts) {
+		this.subtotalotherdiscounts = subtotalotherdiscounts;
+	}
+	public String getTotaldiscounts() {
+		return totaldiscounts;
+	}
+	public void setTotaldiscounts(String totaldiscounts) {
+		this.totaldiscounts = totaldiscounts;
+	}
+	
+
+}
