d8da5af486d767eacd603386dd8ca2231924247c
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@afae724c-4187-47be-f31d-de5aa923d161>
2013-07-11 22:43:04 +0000

    Se avanza con el desarrollo del despliegue de los FK.

--- trunk/remcon-web/src/cl/buildersoft/framework/util/BSGenericModelUtils.java
+++ trunk/remcon-web/src/cl/buildersoft/framework/util/BSGenericModelUtils.java
@@ -5,12 +5,14 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import cl.buildersoft.framework.dataType.BSDataType;
 import cl.buildersoft.framework.dataType.BSDataTypeUtil;
+import cl.buildersoft.framework.database.BSmySQL;
 import cl.buildersoft.framework.exception.BSDataBaseException;
 import cl.buildersoft.framework.exception.BSProgrammerException;
 import cl.buildersoft.framework.util.crud.BSField;
@@ -44,7 +46,7 @@ public abstract class BSGenericModelUtils {
 			field.setType(BSDataTypeUtil.create(BSDataType.TIMESTAMP));
 		} else if (typeName.equals("DOUBLE")) {
 			field.setType(BSDataTypeUtil.create(BSDataType.DOUBLE));
-		}else if (typeName.equals("DECIMAL")){
+		} else if (typeName.equals("DECIMAL")) {
 			field.setType(BSDataTypeUtil.create(BSDataType.DECIMAL));
 		} else if (typeName.equals("BIT")) {
 			field.setType(BSDataTypeUtil.create(BSDataType.BOOLEAN));
@@ -55,6 +57,7 @@ public abstract class BSGenericModelUtils {
 					+ getClass().getName() + ".setRealType()");
 		}
 	}
+
 	protected String getSQLForReadStruct(String databaseName, String[] names, String tableName) {
 		String out = "SELECT ";
 
@@ -68,26 +71,38 @@ public abstract class BSGenericModelUtils {
 		out += " LIMIT 0,1";
 		return out;
 	}
-	
+
 	protected void configFKFields(Connection conn, Map<String, BSField> fields, String tableName) {
 		String databaseFK = null;
 		String tableFK = null;
 		String fieldFK = null;
-		BSField field =null;
-		
+		BSField field = null;
+
 		for (Entry<String, BSField> entry : fields.entrySet()) {
 			/**
-		    System.out.println("Key [" + entry.getKey() + "]");
-		    System.out.println("Value [" + entry.getValue() + "]");
-		    */
-			 field = entry.getValue();
+			 * System.out.println("Key [" + entry.getKey() + "]");
+			 * System.out.println("Value [" + entry.getValue() + "]");
+			 */
+			field = entry.getValue();
 			String[] fkTableInfo = getFKTableInfo(conn, field, tableName);
-			if (fkTableInfo != null) {
-				field.setFK  (fkTableInfo[0], fkTableInfo[1], fkTableInfo[2]);
+			if (fkTableInfo != null && fkTableInfo.length > 0) {
+				databaseFK = fkTableInfo[0];
+				tableFK = fkTableInfo[1];
+				fieldFK = fkTableInfo[2];
+				if (tableFK != null && fieldFK != null) {
+					BSmySQL mysql = new BSmySQL();
+					String sql = "SELECT cId, cName ";
+					sql += "FROM " + databaseFK + ".";
+					sql += field2Table(conn, field.getName(), databaseFK);
+					sql += " ORDER BY cName";
+
+					field.setFkData(mysql.resultSet2Matrix(mysql.queryResultSet(conn, sql, null)));
+				}
 			}
-			
-		}		
-		/**<code>
+
+		}
+		/**
+		 * <code>
 		for (BSField field : fields) {
 			String[] pkTableInfo = getFKTableInfo(conn, field);
 
@@ -108,8 +123,10 @@ public abstract class BSGenericModelUtils {
 				}
 			}
 			
-		}</code>*/
+		}</code>
+		 */
 	}
+
 	private String[] getFKTableInfo(Connection conn, BSField field, String tableName) {
 		/**
 		 * <code>
@@ -119,14 +136,13 @@ public abstract class BSGenericModelUtils {
 		 * PKCOLUMN_NAME=cId
 		 * </code>
 		 */
-		DatabaseMetaData dbmd;
-		ResultSet rs;
-		String[] out = null;
 
-		
-		Map<String, String[]> fkInfo = field.getFkInfo();
-		if (fkInfo== null) {
-			fkInfo = new HashMap<String, String[]>();
+		String[] out = field.getFKInfo();
+
+		if (out == null) {
+			DatabaseMetaData dbmd;
+			ResultSet rs;
+
 			try {
 				dbmd = (DatabaseMetaData) conn.getMetaData();
 				rs = dbmd.getImportedKeys(null, null, tableName);
@@ -136,13 +152,15 @@ public abstract class BSGenericModelUtils {
 					String database = rs.getString("PKTABLE_CAT");
 					String table = rs.getString("PKTABLE_NAME");
 					String column = rs.getString("PKCOLUMN_NAME");
+					
+					if (code.equalsIgnoreCase(field.getName())) {
+						out = new String[3];
+						out[0] = database;
+						out[1] = table;
+						out[2] = column;
 
-					String[] info = new String[3];
-					info[0] = database;
-					info[1] = table;
-					info[2] = column;
-
-					fkInfo.put(code, info);
+						field.setFKInfo(out);
+					}
 				}
 
 			} catch (SQLException e) {
@@ -150,8 +168,81 @@ public abstract class BSGenericModelUtils {
 			}
 		}
 
-		out = fkInfo.get(field.getName());
+		return out;
+	}
+
+	private String field2Table(Connection conn, String fieldName, String database) {
+		String out = null;
+		String table = "t" + fieldName.substring(1);
+		String view = "";
+		Set<String> set = listTableNames(conn, database);
+		if (!set.contains(table)) {
+			view = "v" + fieldName.substring(1);
+
+			if (set.contains(view)) {
+				out = view;
+			}
+		} else {
+			out = table;
+		}
+
+		if (out == null) {
+			throw new BSProgrammerException("", "No existe la tabla '" + table + "' o vista '" + view + "' en la base de datos '"
+					+ database + "'");
+		}
 
 		return out;
 	}
+
+	private Set<String> listTableNames(Connection conn, String database) {
+		Set<String> out = new HashSet<String>();
+
+		DatabaseMetaData dbmd;
+		try {
+			dbmd = (DatabaseMetaData) conn.getMetaData();
+			ResultSet tables = dbmd.getTables(database, null, null, null);
+
+			while (tables.next()) {
+				out.add(tables.getString("TABLE_NAME").toLowerCase());
+			}
+			tables.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return out;
+	}
+
+	/**
+	 * <code>
+	private Boolean isExistInCommon(Connection conn, String table, String database) {
+		if (this.tablesCommon == null) {
+			this.tablesCommon = new HashSet<String>();
+ 
+			Set<String> databases = databasesList(conn);
+
+			try {
+				Iterator<String> dataBasesIter = databases.iterator();
+
+				while (dataBasesIter.hasNext()) {
+					String database = (String) dataBasesIter.next();
+
+					DatabaseMetaData dbmd = (DatabaseMetaData) conn.getMetaData();
+					ResultSet tables = dbmd.getTables(database, null, null, null);
+
+					while (tables.next()) {
+						this.tablesCommon.add(tables.getString("TABLE_NAME").toLowerCase());
+					}
+					tables.close();
+				}
+			} catch (SQLException e) {
+				throw new BSDataBaseException(e);
+			}
+		}
+
+		return this.tablesCommon.contains(table.toLowerCase());
+	}
+	</code>
+	 */
+
 }
