5c4c712d13fa4600b4232a2cfff1d74cbdf17436
claudio.moscoso <claudio.moscoso@gmail.com>
2013-07-22 22:29:11 +0000

    Se avanza en el desarrollo del manejo de los campos del Parent.

--- remcon-web/WebContent/js/framework/parent-child/index.js
+++ remcon-web/WebContent/js/framework/parent-child/index.js
@@ -1,3 +1,17 @@
 function newParent(){
-	alert('in newParent');
+//	alert(document.getElementById('parentForm').childNodes.length);
+//	alert(document.getElementById('parentForm').elements.length);
+	
+	var coll = document.getElementById('parentForm').elements;
+	
+	for(e in coll){
+		alert(e.innerHTML);
+	}
+	
+	/**
+	for(var idx = 0; idx< document.getElementById('parentForm').childNodes.length;idx++){
+		alert(document.getElementById('parentForm').elements.length);
+	}
+	*/
+	
 }
